b5b1ce52ea81250a16f65230476e56c4022c8db7
alexroutledge <alex.routledge@gmail.com>
2015-01-20 23:38:54 +0000

    Update sw.js

--- serviceworker/sw.js
+++ serviceworker/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('fetch', function(event) {
   if (/\.jpg$/.test(event.request.url)) {
     event.respondWith(
-      fetch('https://www.google.co.uk/test.gif', {
+      fetch('https://www.google.co.uk/logos/doodles/2014/60th-anniversary-of-the-unveiling-of-the-first-routemaster-bus-4922931108904960.3-hp.gif', {
         mode: 'no-cors'
       })
     );
