1575d4ad6df977db7e9affbaad64e3caf129d167
Alex Routledge <alex.routledge@gmail.com>
2016-02-03 22:27:18 +0000

    Update lazy-load-images.js

--- serviceworker/lazy-load-images.js
+++ serviceworker/lazy-load-images.js
@@ -1,3 +1,9 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
 // Listen to fetch events
 self.addEventListener('fetch', function(event) {
   if (/\.gif$|.png$/.test(event.request.url)) {
