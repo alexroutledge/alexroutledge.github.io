f2dc6fdf6dee5e3aa51e1e8fe9c62637ebe7d137
Alex Routledge <alex.routledge@gmail.com>
2016-02-03 22:24:46 +0000

    Update lazy-load-images.js

--- serviceworker/lazy-load-images.js
+++ serviceworker/lazy-load-images.js
@@ -1,8 +1,10 @@
 // Listen to fetch events
 self.addEventListener('fetch', function(event) {
+  if (/\.gif$|.png$/.test(event.request.url)) {
   event.respondWith(
 	  fetch('lazy-load.gif', {
 		  mode: 'no-cors'
 		  })
 		);
+  }
 });
